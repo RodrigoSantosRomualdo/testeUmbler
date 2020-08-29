b0b22039639c8d301df55ec5be354d7f0e562a22
Rodrigo Romualdo <rodrigo.romualdo@intelliway.com.br>
2020-08-29 00:49:16 -0300

    segundo mommit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. MEU APP SUBIU
-          Será que vai
+          Primeira vez não foi
         </p>
         <a
           className="App-link"
